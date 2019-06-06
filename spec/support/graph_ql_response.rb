1695842b147bbc12016ca1d2bb9f81514c808967
Don Schrimsher <don4thirteen@gmail.com>
2019-06-06 12:37:00 -0400

    Implement whitelist authentcation
    Implements whitelist authentication and excludes the sign in operation
    from authentication.

--- spec/support/graph_ql_response.rb
+++
@@ -1,9 +0,0 @@
-require 'ostruct'
-
-class GraphQlResponse
-  def self.parse(json_string)
-    snake_case_json = JSON.parse(json_string).deep_transform_keys(&:underscore).to_json
-    json = JSON.parse(snake_case_json, object_class: OpenStruct)
-    OpenStruct.new(json)
-  end
-end
