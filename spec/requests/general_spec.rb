706ceb4e88b34f39c84650f07606bdfddec62add
Don Schrimsher <don4thirteen@gmail.com>
2019-06-07 12:24:29 -0400

    Simplify GraphQL query structure
    Simplifies the GraphQL query structure for the client by reading the
    operation name from the query instead of requiring the client to pass it
    in as a param.

--- spec/requests/general_spec.rb
+++ spec/requests/general_spec.rb
@@ -1,8 +1,23 @@
 RSpec.describe "General API functionality", type: :request do
   it "rejects unauthenticated requests" do
 
-    post '/graphql', params: { query: user_detail_query, operationName: 'userDetail' }
+    post '/graphql', params: { query: user_detail_query }
 
     expect(response.status).to eq(401)
+  end
+
+  it "rejects queries with no operation name" do
+    query = <<-QUERY
+      {
+        userDetail {
+          firstName
+        }
+      }
+    QUERY
+
+    result = graphql(query: query)
+
+    expect(response.status).to eq(422)
+    expect(result.errors).to include("Please provide an operation name for the following query: #{query}")
   end
 end
