59fe49afcf069e29a919a7a4edff460f59088d3a
Don Schrimsher <don4thirteen@gmail.com>
2019-06-20 16:13:47 -0400

    Return user in sign in mutation

--- app/graphql/mutations/sign_in.rb
+++ app/graphql/mutations/sign_in.rb
@@ -5,14 +5,15 @@ class Mutations::SignIn < Mutations::BaseMutation
   argument :email, String, required: true
   argument :password, String, required: true
 
-  type Types::AuthType
+  field :auth, Types::AuthType, null: false
+  field :user, Types::User, null: false
 
   def resolve(email:, password:)
     if user = User.find_for_database_authentication(email: email)
       if user.valid_password?(password)
         user.ensure_authentication_token!
         authentication_token = user.authentication_token
-        return OpenStruct.new(authentication_token: authentication_token)
+        return OpenStruct.new(user: user, auth: { authentication_token: authentication_token })
       else
         GraphQL::ExecutionError.new('Incorrect Email/Password')
       end
--- spec/requests/sign_in_spec.rb
+++ spec/requests/sign_in_spec.rb
@@ -1,13 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "Sign in", type: :request do
-  it "returns an authentication token" do
+  it "returns an authentication token and the user" do
     variables = OpenStruct.new(email: 'someemail@example.com', password: 'password')
     user = create(:user, email: variables.email, password: variables.password)
 
-    result = graphql(query: sign_in_mutation, variables: variables)
+    result = graphql(query: sign_in_mutation, variables: variables).data.sign_in
 
-    expect(result.data.sign_in.authentication_token).to be_truthy
+    expect(result.auth.authentication_token).to be_truthy
+    expect(result.user.id).to eq(user.id.to_s)
   end
 
   it "doesn't allow members to sign in until their account has been confirmed" do
