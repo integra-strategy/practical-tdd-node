a2d745d896e223e182ded481f6c1f9c204312615
Don Schrimsher <don4thirteen@gmail.com>
2019-06-20 16:19:49 -0400

    Skip member and employee confirmation for seed

--- db/seeds/01_create_users.rb
+++ db/seeds/01_create_users.rb
@@ -1,15 +1,19 @@
-Member.create!(
+member = Member.new(
   first_name: "John",
   last_name: "Doe",
   email: "member@example.com",
   password: "password",
   profile_picture: "https://example.com"
 )
+member.skip_confirmation!
+member.save
 
-Employee.create!(
+employee = Employee.new(
   first_name: 'Jane',
   last_name: 'Doe',
   email: 'employee@example.com',
   password: 'password',
   profile_picture: 'https://example.com'
 )
+employee.skip_confirmation!
+empmloyee.save
