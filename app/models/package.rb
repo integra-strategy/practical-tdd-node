34910792adab348a5a1f07b2b69ae1a646dc29a6
Don Schrimsher <don4thirteen@gmail.com>
2019-07-02 16:53:44 -0400

    Add static daily package
    Adds a static daily package. We have to add a static daily package
    because Stripe doesn't support adding a pricing plan that is a 1 time
    charge.

--- app/models/package.rb
+++ app/models/package.rb
@@ -1,11 +1,14 @@
 class Package
+  DAILY_DISPLAY_NAME = 'Daily'
+
   def self.fetch(id)
     return EmptyPackage.new if id.nil?
     new Stripe::Plan.retrieve(id)
   end
 
   def self.fetch_all
-    Stripe::Plan.list(limit: 10).map { |plan| new plan }
+    packages_from_stripe = Stripe::Plan.list(limit: 10).map { |plan| new plan }
+    packages_from_stripe.prepend(daily_package)
   end
 
   def initialize(plan)
@@ -32,5 +35,24 @@ class Package
 
   def metadata
     @plan[:metadata] || {}
+  end
+
+  # We have to add a static daily package because Stripe doesn't
+  # support adding a pricing plan (which is what represents a package
+  # in our system) for a 1 time charge.
+  def self.daily_package
+    new({
+      id: 'daily',
+      amount: 10,
+      metadata: {
+        display_name: DAILY_DISPLAY_NAME,
+        description: [
+          "Provides Full Access to Park",
+          "No Commitment",
+          "Fee Taken at Park",
+          "Perfect for the Out of Towner"
+        ].to_json
+        }
+      })
   end
 end
