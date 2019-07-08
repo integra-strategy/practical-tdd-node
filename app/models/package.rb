3d65a45bc9ba6b15db32bead4093119c37b55c36
Don Schrimsher <don4thirteen@gmail.com>
2019-07-08 18:52:00 -0400

    Remove static Daily package
    Removes the static daily package. It's no longer needed since we now
    support a 1 time daily subscription that cancels automatically.

--- app/models/package.rb
+++ app/models/package.rb
@@ -7,8 +7,7 @@ class Package
   end
 
   def self.fetch_all
-    packages_from_stripe = Stripe::Plan.list(limit: 10).map { |plan| new plan }
-    packages_from_stripe.prepend(daily_package)
+    Stripe::Plan.list(limit: 10).map { |plan| new plan }
   end
 
   def initialize(plan)
@@ -39,24 +38,5 @@ class Package
 
   def metadata
     @plan[:metadata] || {}
-  end
-
-  # We have to add a static daily package because Stripe doesn't
-  # support adding a pricing plan (which is what represents a package
-  # in our system) for a 1 time charge.
-  def self.daily_package
-    new({
-      id: 'daily',
-      amount: 10,
-      metadata: {
-        display_name: DAILY_DISPLAY_NAME,
-        description: [
-          "Provides Full Access to Park",
-          "No Commitment",
-          "Fee Taken at Park",
-          "Perfect for the Out of Towner"
-        ].to_json
-      }
-    })
   end
 end
